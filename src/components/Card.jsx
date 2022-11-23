c3688db9b69b5185d6ed825ab627e917779342e7
Sinan <sinankzu@gmail.com>
2022-11-23 19:06:25 +0100

    son

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,19 +12,15 @@ const Card = () => {
        {isLoading ? <div>Loading</div>:contentList.map((element) => {
 
         return (
-        <div className="card">
-          <img
-            width={300}
-            src={element.image}
-            alt=""
-          />
-          
-            <h3>{element.title}</h3>
-          
-          
-            <p>{element.content}</p>
-          
-        </div>);
+          <div className="card">
+            <img width={400} src={element.image} alt="" />
+            <div className="text-content">
+              <h3>{element.title}</h3>
+
+              <p>{element.content}</p>
+            </div>
+          </div>
+        );
       })} 
     </div>
   );
